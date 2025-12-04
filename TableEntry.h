9d612393019fce6f9a1be1e4503f3ce58f5a3245
Ian Francisco García Ruiz <ifgarrui@etsinf.upv.es>
2025-12-04 12:12:39 +0100

    Update TableEntry.h

--- TableEntry.h
+++ TableEntry.h
@@ -4,11 +4,35 @@
 #include <string>
 #include <ostream>
 
-template <typename V> 
+template <typename V>
 class TableEntry {
-    public:
-        // miembros públicos
-    
+public:
+    // Atributos públicos
+    std::string key;
+    V value;
+
+    // Constructores
+    TableEntry(std::string key, V value) : key(key), value(value) {}
+
+    TableEntry(std::string key) : key(key) {}
+
+    TableEntry() : key("") {}
+
+    // Operador ==
+    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key == te2.key;
+    }
+
+    // Operador !=
+    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key != te2.key;
+    }
+
+    // Operador <<
+    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+        out << "(" << te.key << " -> " << te.value << ")";
+        return out;
+    }
 };
 
 #endif
