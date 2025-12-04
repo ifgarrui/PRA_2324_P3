738cdadd2dd5c3011e091649de056bb7ec2dd192
Ian Francisco García Ruiz <ifgarrui@etsinf.upv.es>
2025-12-04 12:11:23 +0100

    "Añadida implementación de la clase TableEntry"

---
+++ TableEntry.h
@@ -0,0 +1,14 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+
+template <typename V> 
+class TableEntry {
+    public:
+        // miembros públicos
+    
+};
+
+#endif
