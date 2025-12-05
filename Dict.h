67b9c28eb7f0e9ff0e227539f7c30f68134fcafb
Ian Francisco García Ruiz <ifgarrui@etsinf.upv.es>
2025-12-05 17:30:42 +0100

    Update Dict.h

--- Dict.h
+++ Dict.h
@@ -1,11 +1,28 @@
 #ifndef DICT_H
 #define DICT_H
 #include <string>
+#include <stdexcept> // Para std::runtime_error
 
 template <typename V> 
 class Dict {
     public:
-        // ... aquí los métodos virtuales puros
+        // Inserta el par clave->valor en el diccionario
+        // Lanza std::runtime_error si la clave ya existe
+        virtual void insert(std::string key, V value) = 0;
+
+        // Busca el valor asociado a la clave
+        // Lanza std::runtime_error si la clave no existe
+        virtual V search(std::string key) = 0;
+
+        // Elimina el par clave->valor del diccionario
+        // Lanza std::runtime_error si la clave no existe
+        virtual V remove(std::string key) = 0;
+
+        // Devuelve el número de elementos en el diccionario
+        virtual int entries() = 0;
+
+        // Destructor virtual
+        virtual ~Dict() {}
 };
 
 #endif
